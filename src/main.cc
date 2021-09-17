0bdb24fb625a99e65dae1814958df7ee6738f3ac
carolinavillam <carolina.villa08@hotmail.com>
2021-09-17 09:14:07 -0600

    publish

---
+++ src/main.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Node {
+	string dish;
+	Node *next;
+};
+
+void Add(Node *&, string);
+void Out(Node *&, string &);
+int main (){
+	Node *stack = NULL;
+	int flag=1;
+	string dish;
+	do{
+		cout<<"\nDish to add: ";
+		cin>>dish;
+		Add(stack,dish);
+		cout<<"\nDo you want to add more? Yes - 1 \n  No - 2"<<endl;
+		cin>>flag;
+	}while(flag==1);
+	cout<<"\nTaking dishes out of the pile"<<endl;
+	while(stack!=NULL){
+		
+		Out(stack,dish);
+		cout<<dish<<endl;
+	}
+	getch();
+	return 0;
+}
+
+void Add(Node *&stack, string _dish){
+	Node *newNode = new Node();
+	newNode->dish = _dish;
+	newNode->next = stack;
+	stack = newNode;
+	
+	cout<< "\nAdd "<<_dish<<endl;
+}
+
+void Out(Node *&stack, string &_dish){
+	Node *aux = stack;
+	_dish = aux -> dish;
+	stack = aux -> next;
+	delete aux;
+}
